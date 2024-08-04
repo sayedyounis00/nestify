eb266c72aa5a6db5e104f871e23f07f50a2f0612
yousefdewidar <ymahmoud1213@gmail.com>
2024-08-04 07:49:49 +0300

    create files for auth feature

---
+++ lib/features/auth/presentation/view/widgets/forget_pass_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassBody extends StatelessWidget {
+  const ForgetPassBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
