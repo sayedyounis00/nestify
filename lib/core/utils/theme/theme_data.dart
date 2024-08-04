09e65e429da60cd8d758953d4babbde08bf5bbe5
sayed younis <elsaydseif17@gmail.com>
2024-08-04 12:43:07 +0300

    add text theme

--- lib/core/utils/theme/theme_data.dart
+++ lib/core/utils/theme/theme_data.dart
@@ -5,13 +5,16 @@ ThemeData getThemeDataLight() => ThemeData(
       primaryColor: AppColor.primaryColor,
       appBarTheme: appBarTheme(),
       textButtonTheme: textButtonTheme(),
+      textTheme: textTheme(),
     );
 
 TextButtonThemeData textButtonTheme() => const TextButtonThemeData(
       style: ButtonStyle(
-        backgroundColor: WidgetStatePropertyAll(AppColor.primaryColor),
+        foregroundColor: WidgetStatePropertyAll(AppColor.primaryColor),
       ),
     );
 AppBarTheme appBarTheme() => const AppBarTheme(
       backgroundColor: AppColor.primaryColor,
     );
+TextTheme textTheme() =>
+    const TextTheme(titleMedium: TextStyle(color: AppColor.secColor4));
