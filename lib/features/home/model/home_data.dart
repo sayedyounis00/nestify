28273e645ec888311e095c93ffde8709e0aaae8c
sayed younis <elsaydseif17@gmail.com>
2024-08-05 17:43:46 +0300

    home model &data

---
+++ lib/features/home/model/home_data.dart
@@ -0,0 +1,40 @@
+class HomeData {
+  static List wholeList = [
+    [
+      'Tanta, Egypt',
+      'Paris, France',
+      'Cairo, Egypt',
+      'Mansora, Egypt',
+      'Florida, Usa',
+      'Sina, Egypt',
+      'Gaza, palestine',
+    ],
+    [
+      r'$500-1000',
+      r'$1500-2000',
+      r'$2500-3000',
+      r'$3500-4000',
+      r'$4500-5000',
+      r'$5500-6000',
+    ],
+    [
+      'Junior suites',
+      'Presidential suite',
+      'Studio',
+      'Penthouse',
+      'Florida, Usa',
+      'Twin room',
+      'Bridal suites',
+    ],
+    [
+      '1-2',
+      '3-4',
+    ],
+  ];
+  static const List<String> upText = [
+    'Locations',
+    'Price Range',
+    'Type',
+    'Bed',
+  ];
+}
