dd14834286a3319e51db091d86bdb7f7bbf7a799
sayed younis <elsaydseif17@gmail.com>
2024-08-05 17:44:01 +0300

    home view custom widget

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+import 'package:nestify/core/theme/app_color.dart';
+import 'package:nestify/features/home/presentation/views/widgets/location_card.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 150,
+          decoration: const BoxDecoration(color: AppColor.primaryColor),
+        ),
+        Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 50.0, left: 20),
+              child: CircleAvatar(
+                backgroundColor: AppColor.secColor4,
+                child: Icon(Icons.person),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0, left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Hi John,',
+                      style:
+                          Theme.of(context).textTheme.titleSmall!.copyWith(
+                                color: AppColor.secColor3,
+                              )),
+                  Text('Welcome!,',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(color: AppColor.secColor4))
+                ],
+              ),
+            ),
+            const LocationCard(),
+            const Padding(
+              padding: EdgeInsets.only(top: 50.0,left: 20),
+              child: Icon(Icons.notifications_none,size: 30,color: AppColor.secColor4,),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
