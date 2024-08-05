4d7430edc5fce1699a6f0ad5a1899dee15a24353
sayed younis <elsaydseif17@gmail.com>
2024-08-05 19:29:58 +0300

    delete un wanted pages

--- lib/core/utils/router.dart
+++ lib/core/utils/router.dart
@@ -2,39 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:nestify/core/utils/routes.dart';
 import 'package:nestify/features/auth/presentation/view/login_view.dart';
 import 'package:nestify/features/auth/presentation/view/signup_view.dart';
-import 'package:nestify/features/auth/presentation/view/verification_view.dart';
-import 'package:nestify/features/auth/presentation/view/verified_view.dart';
-import 'package:nestify/features/home/presentation/views/home_view.dart';
 import 'package:nestify/features/splash/presentation/view/landing_view.dart';
 import 'package:nestify/features/splash/presentation/view/splash_view.dart';
 
 Route<dynamic> onGenerate(RouteSettings settings) {
   switch (settings.name) {
-    case AddRouter.landingListViewRoute:
-      {
-        return CupertinoPageRoute(builder: (_) => const LandingView());
-      }
     case AddRouter.splashViewRoute:
       return CupertinoPageRoute(builder: (_) => const SplashView());
+    case AddRouter.landingListViewRoute:
+      return CupertinoPageRoute(builder: (_) => const LandingView());
     case AddRouter.loginViewRoute:
       return CupertinoPageRoute(builder: (_) => const LoginView());
     case AddRouter.signupViewRoute:
       return CupertinoPageRoute(builder: (_) => const SignupView());
-      {
-        return CupertinoPageRoute(builder: (_) => const LoginView());
-      }
-    case AddRouter.verificationRoute:
-      {
-        return CupertinoPageRoute(builder: (_) => const VerificationView());
-      }
-    case AddRouter.verifiedRoute:
-      {
-        return CupertinoPageRoute(builder: (_) => const VerifiedView());
-      }
-    case AddRouter.homeView:
-      {
-        return CupertinoPageRoute(builder: (_) => const HomeView());
-      }
+    case AddRouter.homeViewRoute:
+      return CupertinoPageRoute(builder: (_) => const SignupView());
     default:
       return CupertinoPageRoute(builder: (_) => const LandingView());
   }
--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -3,5 +3,8 @@ class AddRouter {
   static const String splashViewRoute = '/splash';
   static const String loginViewRoute = '/loginPage';
   static const String signupViewRoute = '/signupPage';
+  static const String verificationViewRoute = '/verificationPage';
+  static const String verifiedViewRoute = '/VerifiedPage';
+  static const String homeViewRoute = '/HomePage';
 
 }
