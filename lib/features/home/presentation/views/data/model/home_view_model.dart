0eb1f18b4f8611906087e8f2e83bae32dc278a77
sayed younis <elsaydseif17@gmail.com>
2024-08-06 12:46:55 +0300

    adding init pages in nav bar

---
+++ lib/features/home/presentation/views/data/model/home_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:nestify/features/home/presentation/views/widgets/discover_view_body.dart';
+import 'package:nestify/features/home/presentation/views/widgets/fav_view_body.dart';
+import 'package:nestify/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:nestify/features/home/presentation/views/widgets/messanger_view_body.dart';
+
+class HomeViewModel{
+    static final List<Widget> viewsBody = [
+    const HomeViewBody(),
+    const DiscoverViewBody(),
+    const FavViewBody(),
+    const MessangerViewBody(),
+  ];
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nestify/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:nestify/features/home/presentation/views/widgets/drawer/custom_drawer.dart';
 import 'package:nestify/features/home/presentation/views/widgets/search_filter.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,15 +8,18 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Stack(
-          children: [
-            CustomAppBar(),
-            SearchFilter(),
-          ],
-        ),
-      ],
+    return const Scaffold(
+      drawer: CustomDrawer(),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              CustomAppBar(),
+              SearchFilter(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
