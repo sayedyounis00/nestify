6af92c754df66f0674746da5d647df5150aaec6b
yousefdewidar <ymahmoud1213@gmail.com>
2024-08-08 02:52:13 +0300

    End category list view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:nestify/core/theme/app_color.dart';
-import 'package:nestify/core/utils/styles.dart';
+import 'package:flutter/widgets.dart';
+import 'package:nestify/features/home/presentation/views/widgets/category/category_list_view.dart';
+import 'package:nestify/features/home/presentation/views/widgets/category/category_text_row.dart';
 import 'package:nestify/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:nestify/features/home/presentation/views/widgets/search_filter.dart';
 
@@ -18,37 +19,10 @@ class HomeViewBody extends StatelessWidget {
             SearchFilter(),
           ],
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: CategoryTextRow(),
-        ),
-
+        CategoryTextRow(),
+        CategoryListView(),
 
         
-
-      ],
-    );
-  }
-}
-
-class CategoryTextRow extends StatelessWidget {
-  const CategoryTextRow({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text('Categories', style: Styles.style19),
-        TextButton(
-          style: const ButtonStyle(
-              padding: WidgetStatePropertyAll(EdgeInsets.zero)),
-          child: const Text('See all',
-              style: TextStyle(color: AppColor.primaryColor)),
-          onPressed: () {},
-        )
       ],
     );
   }
