a5059755c15a8f28cf1748dbea6732912e2a4d66
yousefdewidar <ymahmoud1213@gmail.com>
2024-08-09 10:52:51 +0300

    v

--- lib/features/auth/presentation/view/widgets/custom_button.dart
+++ lib/features/auth/presentation/view/widgets/custom_button.dart
@@ -8,22 +8,23 @@ class CustomButton extends StatelessWidget {
   final Color color;
   final Color foreCol;
   final double? width;
-  final double ?height;
+  final double? height;
   const CustomButton({
     super.key,
     required this.text,
     required this.onPressed,
     required this.color,
-    this.foreCol = Colors.white,  this.width,  this.height,
+    this.foreCol = Colors.white,
+    this.width,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
         style: ButtonStyle(
-          alignment: Alignment.center,
-          // fixedSize: const WidgetStatePropertyAll(Size(120,50)),
-
+            alignment: Alignment.center,
+            fixedSize: WidgetStatePropertyAll(Size(width ?? 120, height ?? 50)),
             foregroundColor: WidgetStatePropertyAll(foreCol),
             backgroundColor: WidgetStatePropertyAll(color),
             shape: const WidgetStatePropertyAll(RoundedRectangleBorder(
